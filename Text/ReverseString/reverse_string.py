21e4ba5483cadc0e7e2ca2dbde688671c85c7db5
omgscar <56062694+omgscar@users.noreply.github.com>
2019-10-06 19:11:53 +0530

    update reverse_string.py

--- Text/ReverseString/reverse_string.py
+++ Text/ReverseString/reverse_string.py
@@ -1,7 +1,8 @@
 #coding: utf-8
 """
-Reverse a string - Enter a string and the program
-will reverse the string and print out.
+REVERSE A STRING:
+Enter a string and the program
+will reverse the string and print it.
 """
 
 if __name__ == '__main__':
@@ -9,7 +10,7 @@ if __name__ == '__main__':
     # Prompt to the user for the string
     string = raw_input("Please, ingress a string to invert: ")
 
-    # Invert the string using slice syntax [begin:end:step]
+    # Reverse the string using slice syntax [begin:end:step]
     reversed_string = string[::-1]
 
     print "The string %s reversed is %s" % (string, reversed_string)
